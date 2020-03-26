539f1540421f0fe2a7b51e313c76a88f031b5ed0
Teagan Ampe <ampe@usc.edu>
2020-03-25 18:39:51 -0700

    installed local node build and grunt task automator

---
+++ gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin:{
+            target:{
+                files:[{
+                    expand:true,
+                    cwd: 'assets/css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest:'assets/css/',
+                    ext:'.min.css'
+                }]
+            }
+        },
+
+        uglify:{
+            options: {
+                message: false
+            },
+            target:{
+                files:[{
+                    expand:true,
+                    cwd: 'assets/js/',
+                    src: ['*.js', '!*.min.js'],
+                    dest:'assets/js/',
+                    ext:'.min.js'
+                }]
+            }
+        }
+    });
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+
+    grunt.registerTask("default", ['uglify', 'cssmin']);
+}
